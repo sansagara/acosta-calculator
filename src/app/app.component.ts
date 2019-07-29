d8f6abe8dd23765ec514a2c2c9c9716d02c96cc8
Leonel Atencio <leo.atencio@gmail.com>
2019-07-29 10:47:43 -0500

    Fixes de jesus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import { InvoiceModel } from './invoice-model';
 
 @Component({
@@ -6,26 +6,34 @@ import { InvoiceModel } from './invoice-model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  invoice = new InvoiceModel(0,0,0.0635,0,0,0,0, 0)
+
+  invoice = new InvoiceModel(0,0,6.35,0,0,0,0, 0);
+
+  selectText($event) {
+    $event.target.select();
+  }
 
   precioUpdated() {
-    this.invoice.costoManejo = this.invoice.precioVenta * 0.05;
+    this.invoice.costoManejo = + (this.invoice.precioVenta * 0.05).toFixed(2);
     this.manejoUpdated();
   }
 
   manejoUpdated() {
-    this.invoice.impuestoVenta = (this.invoice.costoManejo + this.invoice.precioVenta) * this.invoice.impuesto;
-    this.invoice.valorCompra = this.invoice.precioVenta + this.invoice.costoManejo + this.invoice.impuestoVenta;
+    this.invoice.impuestoVenta = + ((this.invoice.costoManejo + this.invoice.precioVenta) * (this.invoice.impuesto/100)).toFixed(2);
+    this.invoice.valorCompra = + (this.invoice.precioVenta + this.invoice.costoManejo + this.invoice.impuestoVenta).toFixed(2);
     this.depositoUpdated();
   }
 
   depositoUpdated() {
-    this.invoice.balancePagar = this.invoice.valorCompra - (this.invoice.depositoEntregado + this.invoice.depositoAdicional);
+    this.invoice.balancePagar = + (this.invoice.valorCompra - (this.invoice.depositoEntregado + this.invoice.depositoAdicional)).toFixed(2);
   }
 
   impuestoUpdated() {
     this.manejoUpdated();
   }
+
+
 
 }
